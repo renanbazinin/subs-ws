969e125a789911ad9a47228785a7d29f08667c32
renan <renanbazinin2@gmail.com>
2022-06-17 12:28:25 +0300

    try fix

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const mongoose  = require("mongoose")
 
 
 const connectDB =  () =>{
-    const uri = "mongodb://localhost:27017/SubscriptionsDB" // like URL but not Only for browesrs
+    const uri = process.env.MONGODB_URL ||  "mongodb://localhost/SubscriptionsDB" // like URL but not Only for browesrs
     ///option for mongoose
     const options ={
         useNewUrlParser:true,
